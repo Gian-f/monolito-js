43000951532046637d3db6f55b8129890c4f181c
Gian Felipe <gianfelipe87@gmail.com>
2023-06-14 21:05:29 -0300

    fix frontend bugs

--- frontend-js/produto.service.js
+++
@@ -1,7 +0,0 @@
-async function fetchProducts() {
-  const API_URL = "http://localhost:3000/api-docs/Produto";
-  const response = await fetch(API_URL);
-  return response.json();
-}
-
-export { fetchProducts };
